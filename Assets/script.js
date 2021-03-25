7d72c7709b9d7c528fa8add344b357edb8d9e05e
Scott Geleas <scottgeleas97@gmail.com>
2021-03-25 17:27:45 -0400

    javascript commenting

--- Assets/script.js
+++ Assets/script.js
@@ -1,34 +1,40 @@
-// Assignment Code
+// this links the button from html to javascript so when you click generate password javascript starts 
 var generateBtn = document.querySelector("#generate");
 function generatePassword() {
   let length = prompt("Enter the length of your desired password between 8 - 128 characters");
 
+  // length must be between 8 and 128 characters or an alert pops up and run generatePassword again
   if (length < 8 || length > 128) {
     alert("Length must be between 8 and 128")
     generatePassword()
     return
   }
   
+  // these confirms let you choose what characters sets to you in your password
   let lower = confirm("Do you want to include lower case letters?");
   let upper = confirm("Do you want to include upper case letters?");
   let number = confirm("Do you want to include numbers?");
   let symbol = confirm("Do you want to include symbols?");
 
+  // If you dont choose one set It will display a messages and run the generatePassword function again
   if (lower === false && upper === false && number === false && symbol === false) {
-    alert("YYou must choose at least one set.")
+    alert("You must choose at least one set.")
     generatePassword()
     return
   }
 
   let myPassword = "";
 
+  // these constants hold arrays of characters by type 
   const lowerCaseLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", ];
   const upperCaseLetters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", ];
   const numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
   const symbols = ["!", ",", "$", "%", "&", "'", "\"" , "\\" , "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "]", "^", "_", "`", "{", "|", "}", "~", ];
 
+  // selectedSet hold all character selections made by the user
   let selectedSet = [];
 
+  // If (__) = true / is selected on the webpage, the set containing the characters is added to selectedSet
   if (lower) {
     selectedSet = selectedSet.concat(lowerCaseLetters);
   }
@@ -45,6 +51,7 @@ function generatePassword() {
     selectedSet = selectedSet.concat(symbols);
   }
 
+  // this for loop cycles through the selectedSet sets of characters  and randomly picks one from each set until the entered length is met
   for (let i = 0; i < length; i++) {
     myPassword += selectedSet[Math.floor(Math.random() * selectedSet.length)];
   }
@@ -52,13 +59,13 @@ function generatePassword() {
   return myPassword;
 }
 
-// Write password to the #password input
+// writes password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
   passwordText.value = password;
 }
 
-// Add event listener to generate button
+// adds event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
